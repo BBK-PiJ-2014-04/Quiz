b4f7febe54f118c0da3e595daf7dbecd3b12e0d3
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-04-22 23:28:11 +0100

    Adding properties and methods for the interface Question

--- interfaces/Question.java
+++ interfaces/Question.java
@@ -4,9 +4,18 @@
 package interfaces;
 
 /**
- * @author MscDevelopment
+ * @author SalvatoreCardali
  *
  */
 public interface Question {
+	public int QuestionID = -1;
+	public String Text = "";
+	
+	public void setQuestionText(String text);
+	public String getQuestionText();
+	public int addAnswer(String text, boolean correct);
+	
+	
+	
 
 }
