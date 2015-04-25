2c3fe4a97de38ae531e32fa2dd62a987d8a318b2
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-04-25 16:16:14 +0100

    A few changes and the Javadoc

--- interfaces/Quiz.java
+++ interfaces/Quiz.java
@@ -2,10 +2,39 @@ package interfaces;
 
 public interface Quiz {
 
+	/**
+	 * Add a question to the list
+	 * 
+	 * @param newQuestion
+	 */
 	public void addQuestion(Question newQuestion);
-	public void removeQuestion(int id);
-	public void getQuizCreator();
+	/**
+	 * Remove a question from the list by giving the question ID.
+	 * 
+	 * @param id
+	 * @return true if the question has been successfully removed, false if it couldn't been found
+	 */
+	public boolean removeQuestion(int id);
+	/**
+	 * Gets all the questions that have been previously added to the Quiz List
+	 */
 	public void getQuestionList();
+	/**
+	 * Gets the name of the Quiz
+	 */
+	public void getQuizName();
+	/**
+	 * Sets the name of the Quiz
+	 */
+	public void setQuizName();
+	/**
+	 * Sets an initial message to show when a player starts to play
+	 * 
+	 * @param Text
+	 */
 	public void setInitialMessage(String Text);
+	/**
+	 * Gets the initial message that has been set up
+	 */
 	public void getInitialMessage();
 }
