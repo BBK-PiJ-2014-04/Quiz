d43628de3324c03c0a60b67415151aefbc8e9894
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-05-03 15:10:53 +0100

    Fixing exceptions

--- server/UserImpl.java
+++ server/UserImpl.java
@@ -1,12 +1,18 @@
 package server;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 import interfaces.User;
 
-public class UserImpl implements User {
+public class UserImpl extends UnicastRemoteObject implements User {
+	/**
+	 * 
+	 */
 	private String name;
 	private int id;
 	
-	public UserImpl(String name, int id) {
+	public UserImpl(String name, int id) throws RemoteException {
 		this.name = name;
 		this.id = id;
 	}
